2f1b76348ca61a26f416685fdd453154f28ae4de
ddean44523 <Daniel.spamforit.dean@gmail.com>
2022-01-29 06:43:39 -0500

    Copied the whol lexer class... have not tested it

--- htmlParser-1.py
+++ htmlParser-1.py
@@ -1,3 +1,9 @@
+
+INT, FLOAT, ID, SEMICOLON, ASSIGNMENTOP, EOI, INVALID = 1, 2, 3, 4, 5, 6, 7
+LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+DIGITS = "0123456789"
+
+
 class Parser:
 
     # WEBPAGE -> <body> { TEXT } </body>
@@ -18,8 +24,56 @@ class Token:
 
 class Lexer:
     def __init__(self, s):
-        self.s = s
+        self.stmt = s
+        self.index = 0
+        self.nextChar()
+
+    def nextChar(self):
+        self.ch = self.stmt[self.index]
+        self.index = self.index + 1 
 
     def nextToken(self):
-        # returns a token Object
-        pass
+        while True:
+            if self.ch.isalpha():
+                id = self.consumeChars(LETTERS+DIGITS)
+                return Token(ID,id)
+            elif self.ch.isdigit():
+                num = self.consumeChars(DIGITS)
+                if self.ch != ".":
+                    return Token(INT, num)
+                num += self.ch
+                self.nextChar()
+                if self.ch.isdigit(): 
+                    num += self.consumeChars(DIGITS)
+                    return Token(FLOAT, num)
+                else: return Token(INVALID, num)
+            elif self.ch==' ': 
+                self.nextChar()
+            elif self.ch==';': 
+                self.nextChar()
+                return Token(SEMICOLON, "")
+            elif self.ch==":":
+                self.nextChar()
+                if self.checkChar("="):
+                    return Token(ASSIGNMENTOP, "")
+                else: return Token(INVALID, "")
+            elif self.ch=='$':
+                return Token(EOI,"")
+            else:
+                self.nextChar()
+                return Token(INVALID, self.ch)
+    
+    def consumeChars(self,charSet):
+        r = self.ch
+        self.nextChar()
+        while (self.ch in charSet):
+            r = r + self.ch
+            self.nextChar()
+        return r
+    
+    def checkChar(self, c):
+        if (self.ch==c):
+            self.nextChar()
+            return True
+        return False
+
